3424c9217ca8bbce0885b581ccad1721834b95ea
mt.louzada <matheuslouzadaa@gmail.com>
2025-02-25 16:45:37 -0300

    feat: insert values

--- aula3/aula03.cs
+++ aula3/aula03.cs
@@ -5,6 +5,9 @@ class Aula03 {
     static void Main(){
         int num1, num2, res;
 
+        num1 = 10;
+        num2 = 2;
+
         Console.WriteLine("A multiplicação de " + num1 + " por " + num2 + " é igual a " + res);
     }
 
