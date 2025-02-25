bd56eb6cc80a488743e5af55362b2d3f7b9b79f5
mt.louzada <matheuslouzadaa@gmail.com>
2025-02-25 16:35:20 -0300

    first commit

---
+++ aula3/aula03.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Aula03 {
+
+    static void Main(){
+        int num1, num2, res;
+
+        Console.WriteLine("A multiplicação de " + num1 + " por " + num2 + " é igual a " + res);
+    }
+
+}
