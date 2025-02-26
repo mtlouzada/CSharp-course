e37e9a7672ec4367f5bb5676763b81d64de8744c
mtlouzada <matheuslouzadaa@gmail.com>
2025-02-25 23:01:50 -0300

    🚀 feat: created an executable for Lesson 3

---
+++ aula3/aula3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Aula03 {
+
+    static void Main(){
+        int num1, num2;
+
+        num1 = 10;
+        num2 = 2;
+
+        Console.WriteLine("A multiplicação de " + num1 + " por " + num2 + " é igual a " + num1 * num2);
+    }
+
+}
