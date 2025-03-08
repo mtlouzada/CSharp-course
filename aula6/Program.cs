d16cc53ecf9a3044f7e41edc176ae821d8d74f35
mtlouzada <matheuslouzadaa@gmail.com>
2025-03-07 22:15:47 -0300

    🎉 feat: started lesson 6 and practiced console formatting

---
+++ aula6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Aula06
+{
+    static void Main()
+    {
+        double valorCompra = 5.50;
+        double valorVenda;
+        double lucro = 0.1;
+        string produto = "Pastel";
+
+        valorVenda = valorCompra + (valorCompra * lucro);
+
+        Console.WriteLine("Produto..........: {0,15}", produto);
+        Console.WriteLine("Valor de compra..: {0,15:c}", valorCompra);
+        Console.WriteLine("Lucro............: {0,15:p}", lucro);
+        Console.WriteLine("Valor de venda...: {0,15:c}", valorVenda);
+    }
+}
---
+++ aula6/README.md
@@ -0,0 +1,5 @@
+# Aula 6 by CFB Cursos
+
+### Resumo:
+
+Nesta aula, revisei formatações e saídas no console em C# e realizei testes práticos para reforçar o aprendizado.
