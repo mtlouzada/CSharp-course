b67bc281a3b546438336fb454c3d7e53492a8f86
mtlouzada <matheuslouzadaa@gmail.com>
2025-03-08 20:14:10 -0300

    🎯 feat: implemented C# code to read input from the keyboard

--- aula8/Program.cs
+++ aula8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Aula8
+{
+    static void Main()
+    {
+        int v1, v2, soma;
+        string nome;
+
+        Console.Write("Digite seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Nome digitado: {0}", nome);
+}
+}
